06777db206c4ae5037fe825fd6aa844482a809c0
sasasukeee <134790706+sasasukeee@users.noreply.github.com>
2024-02-13 21:32:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,139 +1,149 @@
-import { WebSocketServer } from 'ws';
-import { WebSocket } from "ws";
-import { randomBytes } from "crypto";
+    import { WebSocketServer } from 'ws';
+    import { WebSocket } from "ws";
+    import { randomBytes } from "crypto";
 
-let socket;
-let killed = false;
-let stoled_token = false;
+    let socket;
+    let killed = false;
+    let stoled_token = false;
 
-function createSocket(token, tokenid, recaptcha, CUTHOST) {
-    try {
-        console.log("created Socket")
-        socket = new WebSocket("wss://" + CUTHOST, {
-            headers: {
-                CUTHOST,
-                "connection": "Upgrade",
-                "pragma": "no-cache",
-                "cache-control": "no-cache",
-                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-                "upgrade": "websocket",
-                "origin": "https://" + CUTHOST + "/",
-                "sec-websocket-version": "13",
-                "accept-encoding": "gzip, deflate, br",
-                "accept-language": "en-US,en;q=0.9",
-                "sec-websocket-key": randomBytes(16).toString("base64"),
-                "sec-websocket-extensions": "permessage-deflate; client_max_window_bits"
-            }
-        });
+    function createSocket(token, tokenid, recaptcha, CUTHOST, serverobject1) {
+        try {
+            console.log("created Socket")
+            socket = new WebSocket("wss://" + CUTHOST, {
+                headers: {
+                    CUTHOST,
+                    "connection": "Upgrade",
+                    "pragma": "no-cache",
+                    "cache-control": "no-cache",
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                    "upgrade": "websocket",
+                    "origin": "https://" + CUTHOST + "/",
+                    "sec-websocket-version": "13",
+                    "accept-encoding": "gzip, deflate, br",
+                    "accept-language": "en-US,en;q=0.9",
+                    "sec-websocket-key": randomBytes(16).toString("base64"),
+                    "sec-websocket-extensions": "permessage-deflate; client_max_window_bits"
+                }
+            });
 
-        socket.binaryType = "arraybuffer";
+            socket.binaryType = "arraybuffer";
 
-        joinToken(token, tokenid, recaptcha)
+            joinToken(token, tokenid, recaptcha,serverobject1)
 
-    } catch(e) {
-        console.log(e);
-    }
-}
-
-
-function joinToken(token, tokenid, recaptcha) {
-    try {
-        socket.onopen = function (event) {
-            console.log("\x1b[34m"+"Joined Token: ", "\x1b[34m" + "\x1b[33m",token,"\x1b[33m","\x1b[33m",tokenid,"\x1b[33m");
-            socket.send(JSON.stringify([
-                "Epo",
-                2120,
-                1280,
-                52,
-                token,
-                tokenid,
-                0,
-                0,
-                0,
-                0,
-                0,
-                1,
-                0,
-                0,
-                0,
-                null,
-                "🎁",
-                recaptcha
-            ]));
-
-
+        } catch(e) {
+            console.log(e);
         }
+    }
 
-        socket.onmessage = function (event) {
 
-            if (typeof event.data == "string") {
-                let msg = JSON.parse(event.data);
-            } else {
-                let ui8 = new Uint8Array(event.data);
-                switch (ui8[0]) {
-                    case 16:
-                        console.log("Heal:","\x1b[32m",((ui8[1]) * 2),"\x1b[32m"+ "-", "Food:","\x1b[32m",(ui8[2]),"\x1b[32m"+ "-","Warm:", "\x1b[32m",((ui8[3]) * 2),"\x1b[32m" +"-", "Water:", "\x1b[32m",(ui8[4]),"\x1b[32m" )
-                        if(ui8[2] < 80) {
-                            socket.send(JSON.stringify([5, 104]))
-                        }
-                        break;
-                    case 25:
-                        // killed
-                        killed = true
+    function joinToken(token, tokenid, recaptcha, serverobject1) {
+        try {
+            socket.onopen = function (event) {
+                console.log(`Joined Token:
+            Token:   ${token}
+            TokenId: ${tokenid}
+            Mode:    ${serverobject1.gm}
+            Server:  ${serverobject1.a}
+            Players: ${serverobject1.nu}`);
+                socket.send(JSON.stringify([
+                    "Epo",
+                    2120,
+                    1280,
+                    52,
+                    token,
+                    tokenid,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    1,
+                    0,
+                    0,
+                    0,
+                    null,
+                    "🎁",
+                    recaptcha
+                ]));
 
-                        break;
-                    case 30:
-                        // stoled token
-                        stoled_token = true;
-                        break;
+
+            }
+
+            socket.onmessage = function (event) {
+
+                if (typeof event.data == "string") {
+                    let msg = JSON.parse(event.data);
+                } else {
+                    let ui8 = new Uint8Array(event.data);
+                    switch (ui8[0]) {
+                        case 16:
+                            console.log(`Token Info
+            Heal:  ${(ui8[1] * 2)}
+            Food:  ${(ui8[2])}
+            Warm:  ${(ui8[3] * 2)}
+            Water: ${(ui8[4])}`)
+                            if(ui8[2] < 80) {
+                                socket.send(JSON.stringify([5, 104]))
+                            }
+                            break;
+                        case 25:
+                            // killed
+                            killed = true
+
+                            break;
+                        case 30:
+                            // stoled token
+                            stoled_token = true;
+                            break;
+                    }
                 }
             }
-        }
-        // socket.onerror = function (event) {
-        //     console.log("socket error");
-        // }
-        
-        socket.onclose = function () {
-            console.log("socket closed")
-            if(!killed && !stoled_token)
-                joinToken(token, tokenid, recaptcha)
-        }
+            // socket.onerror = function (event) {
+            //     console.log("socket error");
+            // }
+            
+            socket.onclose = function () {
+                console.log("socket closed")
+                if(!killed && !stoled_token)
+                    joinToken(token, tokenid, recaptcha)
+            }
 
 
-    } catch (e) {
-        console.log(e);
+        } catch (e) {
+            console.log(e);
+        }
     }
-}
 
 
 
 
-let STARVE_TOKEN = "";
-let STARVE_TOKEN_ID = "";
+    let STARVE_TOKEN = "";
+    let STARVE_TOKEN_ID = "";
+    let SERVER_OBJECT;
 
+    let port = 8080;
+    const wss = new WebSocketServer({ port: port });
 
-let port = 8080;
-const wss = new WebSocketServer({ port: port });
+    console.log("\x1b[33m%s\x1b[0m","listening on port " + port,"\x1b[33m\x1b[0m");
+    wss.on('connection', function connection(ws) {
+        console.log("Connected to script");
 
-console.log("listening on port " + "\x1b[36m",port,"\x1b[36m");
-wss.on('connection', function connection(ws) {
-    console.log("\x1b[31m"+"Connected to script" , "\x1b[31m");
+        ws.on('message', function message(data) {
+            let packet = JSON.parse(data);
+            switch (packet[0]) {
+                case "join":
+                    let recaptcha = packet[1];
+                    let RAWHOST = packet[2];
+                    let CUTHOST = packet[3];
+                    createSocket(STARVE_TOKEN, STARVE_TOKEN_ID, recaptcha, CUTHOST, SERVER_OBJECT);
+                    // createBot(STARVE_TOKEN, STARVE_TOKEN_ID, recaptcha, RAWHOST, CUTHOST)
+                    break;
+                case "tokens":
+                    STARVE_TOKEN = packet[1];
+                    STARVE_TOKEN_ID = packet[2];
+                    SERVER_OBJECT = packet[3];
+                    break;
+            }   
 
-    ws.on('message', function message(data) {
-        let packet = JSON.parse(data);
-        switch (packet[0]) {
-            case "join":
-                let recaptcha = packet[1];
-                let RAWHOST = packet[2];
-                let CUTHOST = packet[3];
-                createSocket(STARVE_TOKEN, STARVE_TOKEN_ID, recaptcha, CUTHOST)
-                // createBot(STARVE_TOKEN, STARVE_TOKEN_ID, recaptcha, RAWHOST, CUTHOST)
-                break;
-            case "tokens":
-                STARVE_TOKEN = packet[1];
-                STARVE_TOKEN_ID = packet[2];
-                break;
-        }   
-
+        });
     });
-});
